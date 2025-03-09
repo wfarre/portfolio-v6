b1625825b3c57265db56d2bdfa34ca7d4558f4ac
wfarre <william.farre@gmail.com>
2025-03-09 22:16:00 +0800

    fix: navbar animation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import React, { useRef, useState } from "react";
 
 const navLinks = [
   {
@@ -21,8 +23,17 @@ const navLinks = [
 
 const Navbar = () => {
   const [currentLink, setCurrentLink] = useState("home");
+  const navbarRef = useRef();
+  useGSAP(() => {
+    gsap.from(navbarRef.current, {
+      y: -100,
+      opacity: 0,
+      duration: 2,
+      delay: 1.2,
+    });
+  });
   return (
-    <nav className="pt-6 md:pt-20 ">
+    <nav className="pt-6 md:pt-20 " ref={navbarRef}>
       <ul className="flex gap-5 md:gap-8 justify-center py-2 px-4 w-fit mx-auto rounded-full bg-white/20  backdrop-blur-2xl ">
         {navLinks.map((link, index) => {
           return (
